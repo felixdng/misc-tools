9f8f93e9f929665ba845e4eb8ee264cf0acefec7
felixdng <felixdng@gmail.com>
2019-03-06 02:24:44 -0800

    update svn.sh

--- svn.sh
+++ svn.sh
@@ -6,46 +6,52 @@ SVN_COMMIT_LOG=""
 SVN_USER="admin"
 SVN_DATE=`date "+%Y-%m-%d"`
 
-function help() {
-    echo "Usage: $0 [option]"
-    echo "    option: --log \"xxx\"             : commit log message"
-    echo "            --user \"username\"       : user name"
-    echo "            --ignore                : ignore some files."
-	echo "            --test                  : just a test for svn commit command."
-	echo "            --help or -h            : for help on a specific command."
-    echo ""
+function help()
+{
+  echo "Usage: $0 [option]"
+  echo "  option: -m \"xxx\"        : svn commit log message"
+  echo "          -u \"username\"   : user name"
+  echo "          -i              : ignore some files."
+  echo "          -t              : just a test for svn commit command."
+  echo "          -h              : for help on a specific command."
+  echo ""
 }
 
 ## start
 if [ $# -lt 1 ];then
-    help
-    exit
+  help
+  exit
 fi
 
-while [ $# -ge 1 ] ; do
-    case "$1" in
-            --log) SVN_COMMIT_LOG=$2; shift 2;;
-            --user) SVN_USER=$2; shift 2;;
-			--ignore) IGNORE=1; shift 1;;
-			--test) TEST=1; shift 1;;
-			--help | -h) help; exit 0 ; break;;
-			*) echo "unknown parameter $1."; help; exit 1 ; break;;
-    esac
+while getopts ":m:u:ith" opt; do
+  case $opt in
+    m) SVN_COMMIT_LOG=$OPTARG
+       ;;
+    u) SVN_USER=$OPTARG
+       ;;
+    i) IGNORE=1
+       ;;
+    t) TEST=1
+       ;;
+    h) help; exit 0;
+       ;;
+    \?) echo "Invalid option: -$OPTARG"; help; exit 1;
+       ;;
+  esac
 done
 
 if [ -z "${SVN_COMMIT_LOG}" ];then
-    echo "Without log?!"
-    exit
+  echo "Without log?!"
+  exit 1
 fi
 
 if [ 0 -eq ${IGNORE} ];then
-    UNKNOW_FILES=`svn st | awk '{ if($1 != "A" && $1 != "M" && $1 != "D") { print $2 } }'`
-    if [ -n "$UNKNOW_FILES" ];
-    then
-        echo "Info: Some files are not versioned. Run command: 'svn status' to check them."
-        echo ${UNKNOW_FILES}
-        exit
-    fi
+  UNKNOW_FILES=`svn st | awk '{ if($1 != "A" && $1 != "M" && $1 != "D") { print $2 } }'`
+  if [ -n "$UNKNOW_FILES" ];then
+    echo "Info: Some files are not versioned. Run command: 'svn status' to check them."
+    echo ${UNKNOW_FILES}
+    exit
+  fi
 fi
 
 COMMIT_FILES=`svn st | awk 'BEGIN{ORS=" "}{if($1=="A" || $1=="M" || $1=="D") {print $2}}END{printf "\n"}'`
@@ -64,7 +70,8 @@ svn commit ${COMMIT_FILES} -m "[修改原因] $SVN_COMMIT_LOG
 fi
 
 else
-    echo "nothing to be commited"
+  echo "nothing to be commited"
+
 fi
 
 ## end
